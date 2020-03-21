b9713169d09183e12b33f6a0e8dbb86552b4f4e8
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2020-03-21 13:43:30 +0200

    ERB to Haml

--- index.rb
+++ index.rb
@@ -1,19 +1,20 @@
 require 'sinatra'
+require 'haml'
 require './autoload.rb'
 
 set :views, settings.root + '/app/views'
 
 get '/' do
-  erb 'cars/index'.to_sym
+  haml 'cars/index'.to_sym
 end
 
 get '/:brand' do
   cars = CarsParser.call("https://losangeles.craigslist.org/search/cto?auto_make_model=#{params[:brand]}").cars_list
-  erb 'cars/show'.to_sym, locals: { cars: cars }
+  haml 'cars/show'.to_sym, locals: { cars: cars }
 end
 
 get '/:brand/:model' do
   selected = [params[:brand], params[:model]].join('+')
   cars = CarsParser.call("https://losangeles.craigslist.org/search/cto?auto_make_model=#{selected}").cars_list
-  erb 'cars/show'.to_sym, locals: { cars: cars }
+  haml 'cars/show'.to_sym, locals: { cars: cars }
 end
