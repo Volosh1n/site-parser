dd5861d80d53084622d43a8f96ceb0db12e68670
Voloshyn Vsevolod <seva.voloshin@gmail.com>
2020-03-23 00:02:43 +0200

    Specs added (#4)

--- app/services/cars_parser.rb
+++ app/services/cars_parser.rb
@@ -1,5 +1,5 @@
 class CarsParser < BaseParser
-  def cars_list
+  def call
     doc.css('p.result-info').map do |node|
       {
         label: first_child_text(node, 'a.hdrlnk'),
@@ -11,15 +11,15 @@ class CarsParser < BaseParser
 
   private
 
+  def first_child(node, selector)
+    node.elements.css(selector).first
+  end
+
   def first_child_text(node, selector)
     first_child(node, selector).text
   end
 
   def first_child_link(node, selector)
     first_child(node, selector).attributes['href'].value
-  end
-
-  def first_child(node, selector)
-    node.elements.css(selector).first
   end
 end
