81ad157f080fbdfffa28210f1f1653708d9e3167
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2020-06-06 19:16:05 +0300

    Get rid of parsing current path

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 class App < Sinatra::Base
   set :views, settings.root + '/app/views'
 
-  before do
-    Dispatcher.new(request.path).call.map { |key, value| instance_variable_set(key, value) }
-  end
-
   get '/' do
+    Dispatcher.new(params).call.map(&method(:instance_variable_set))
     haml 'cars/index'.to_sym
   end
 
   get '/:brand' do
+    Dispatcher.new(params).call.map(&method(:instance_variable_set))
     haml 'cars/show'.to_sym
   end
 
   get '/:brand/:model' do
+    Dispatcher.new(params).call.map(&method(:instance_variable_set))
     haml 'cars/show'.to_sym
   end
 end
